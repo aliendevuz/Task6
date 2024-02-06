31cef9205df9167e0223ac6e4cd9ff731850df76
aliendevuz <aliendevuz@gmail.com>
2024-02-06 12:31:12 +0500

    4.1 task

--- app/src/main/java/uz/alien/task/MainActivity.kt
+++ app/src/main/java/uz/alien/task/MainActivity.kt
@@ -1,13 +1,7 @@
 package uz.alien.task
 
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import uz.alien.task.databinding.ActivityMainBinding
 import java.io.BufferedReader
 import java.io.File
@@ -20,90 +14,100 @@ import java.nio.charset.Charset
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
+    var isCache = true
+    val fileName = "data.txt"
+    val cacheData = "Wrote to Caches!"
+    val fileData = "Wrote to Files!"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bButton.setOnClickListener {
+        val cacheFile = File(cacheDir, fileName)
+        val fileFile = File(filesDir, fileName)
 
-            permissionLauncher.launch(arrayOf(
-                Manifest.permission.CAMERA,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ))
-
-            val fileName = "data.txt"
-
-            val cacheFile = File(externalCacheDir, fileName)
-            val cacheData = "Hello from Caches!"
-            createFile(cacheFile)
-            saveData(cacheFile, cacheData)
-            binding.tvCaches.text = readData(cacheFile)
-            deleteFile(cacheFile)
-
-            val file = File(getExternalFilesDir(null), fileName)
-            val fileData = "Hello from Files!"
-            createFile(file)
-            saveData(file, fileData)
-            binding.tvFiles.text = readData(file)
-            deleteFile(file)
+        binding.scCache.setOnCheckedChangeListener { buttonView, isChecked ->
+            isCache = isChecked
         }
+
+        binding.bCreate.setOnClickListener {
+            createFile(if (isCache) cacheFile else fileFile)
+        }
+
+        binding.bWrite.setOnClickListener {
+            writeData(if (isCache) cacheFile else fileFile, if (isCache) cacheData else fileData)
+        }
+
+        binding.bRead.setOnClickListener {
+            binding.tvData.text = readData(if (isCache) cacheFile else fileFile)
+        }
+
+        binding.bDelete.setOnClickListener {
+            deleteFile(if (isCache) cacheFile else fileFile)
+        }
+
+
+//        .setOnClickListener {
+//
+//            permissionLauncher.launch(arrayOf(
+//                Manifest.permission.CAMERA,
+//                Manifest.permission.ACCESS_FINE_LOCATION,
+//                Manifest.permission.ACCESS_COARSE_LOCATION,
+//                Manifest.permission.READ_EXTERNAL_STORAGE,
+//                Manifest.permission.WRITE_EXTERNAL_STORAGE
+//            ))
+//        }
     }
 
-    val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {}
+//    val permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {}
+
+    fun setStatus(status: String) {
+        binding.tvStatus.text = status
+    }
 
     fun createFile(file: File) {
         if (!file.exists()) {
-            if (file.createNewFile()) Toast.makeText(this, "Creating: File creation successfully", Toast.LENGTH_SHORT).show()
-            else Toast.makeText(this, "Creating: File creation failed", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Creating: File already created", Toast.LENGTH_SHORT).show()
-        }
+            if (file.createNewFile()) setStatus("Creating: File creation successfully\n${file.absolutePath}")
+            else setStatus("Creating: File creation failed\n${file.absolutePath}")
+        } else setStatus("Creating: File already created\n${file.absolutePath}")
     }
 
-    fun saveData(file: File, data: String) {
-        if (file.exists()) {
-            try {
-                val fileOutputStream = FileOutputStream(file)
-                fileOutputStream.write(data.toByteArray(Charset.forName("UTF-8")))
-                Toast.makeText(this, "Writing: File writing successfully", Toast.LENGTH_SHORT).show()
-            } catch (e: IOException) {
-                Toast.makeText(this, "Writing: Something is wrong", Toast.LENGTH_SHORT).show()
-            }
-        } else Toast.makeText(this, "Writing: File is not exist", Toast.LENGTH_SHORT).show()
+    fun writeData(file: File, data: String) {
+        if (file.exists()) try {
+            val fileOutputStream = FileOutputStream(file)
+            fileOutputStream.write(data.toByteArray(Charset.forName("UTF-8")))
+            setStatus("Writing: File writing successfully\n${file.absolutePath}")
+        } catch (e: IOException) {
+            setStatus("Writing: Something is wrong\n${file.absolutePath}")
+        } else setStatus("Writing: File is not exist\n${file.absolutePath}")
     }
 
     fun readData(file: File): String {
         var data = ""
-        if (file.exists()) {
-            try {
-                val fileInputStream = FileInputStream(file)
-                val reader = BufferedReader(InputStreamReader(fileInputStream, "UTF-8"))
-                var line: String?
-                val lines = ArrayList<String>()
-                while (reader.readLine().also { line = it } != null) lines.add(line!!)
-                reader.close()
-                fileInputStream.close()
-                Toast.makeText(this, "Reading: File reading successfully", Toast.LENGTH_SHORT).show()
-                for (i in 0 until lines.size) {
-                    data += lines[i]
-                    if (i != lines.size - 1) data += "\n"
-                }
-            } catch (e: IOException) {
-                Toast.makeText(this, "Reading: Something is wrong", Toast.LENGTH_SHORT).show()
+        if (file.exists()) try {
+            val fileInputStream = FileInputStream(file)
+            val reader = BufferedReader(InputStreamReader(fileInputStream, "UTF-8"))
+            var line: String?
+            val lines = ArrayList<String>()
+            while (reader.readLine().also { line = it } != null) lines.add(line!!)
+            reader.close()
+            fileInputStream.close()
+            setStatus("Reading: File reading successfully\n${file.absolutePath}")
+            for (i in 0 until lines.size) {
+                data += lines[i]
+                if (i != lines.size - 1) data += "\n"
             }
-        } else Toast.makeText(this, "Reading: File is not exist", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            setStatus("Reading: Something is wrong\n${file.absolutePath}")
+        } else setStatus("Reading: File is not exist\n${file.absolutePath}")
         return data
     }
 
     fun deleteFile(file: File) {
         if (file.exists()) {
-            if (file.delete()) Toast.makeText(this, "Deleting: File removing successfully", Toast.LENGTH_SHORT).show()
-            else Toast.makeText(this, "Deleting: File removing failed", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(this, "Deleting: File doesn't exist", Toast.LENGTH_SHORT).show()
-        }
+            if (file.delete()) setStatus("Deleting: File removing successfully\n${file.absolutePath}")
+            else setStatus("Deleting: File removing failed\n${file.absolutePath}")
+        } else setStatus("Deleting: File doesn't exist\n${file.absolutePath}")
     }
 }
