65d1d9fffea9657a344334a2139e27b98747105c
aliendevuz <aliendevuz@gmail.com>
2024-02-14 12:16:39 +0500

    5.1 task reviewed

--- app/src/main/java/uz/alien/task/MainActivity.kt
+++ app/src/main/java/uz/alien/task/MainActivity.kt
@@ -8,16 +8,14 @@ import uz.alien.task.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityMainBinding
-    lateinit var musicService: Intent
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        musicService = Intent(this, MusicService::class.java)
 
         binding.bStart.setOnClickListener {
-            startService(intent)
+            startService(Intent(this, MusicService::class.java))
         }
     }
 }
